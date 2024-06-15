89af987cf931b6fb6d04e14b68786abc181c2022
Martin A <martin.apalkov@wisercat.eu>
2024-06-15 12:26:55 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Weather Scheduler
+
---
+++ src/main/java/weatherscheduler/WeatherSchedulerApplication.java
@@ -0,0 +1,13 @@
+package weatherscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSchedulerApplication.class, args);
+    }
+
+}
