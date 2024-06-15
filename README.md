afeb6eb6555dac447c2ee94f07edba98d5b4bcfe
Martin A <martin.apalkov@wisercat.eu>
2024-06-15 13:10:33 +0300

    db connection and entity

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Weather Scheduler
 
+psql -U postgres
+CREATE DATABASE weather;
+CREATE USER weather WITH PASSWORD 'weather';
+GRANT ALL PRIVILEGES ON DATABASE weather TO weather;
---
+++ src/main/java/weatherscheduler/entity/WeatherData.java
@@ -0,0 +1,18 @@
+package weatherscheduler.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class WeatherData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String location;
+    private String temperature;
+    private LocalDateTime timestamp;
+}
